b59a0f5f301e09be84323e67af2ea36624eb8969
seekr123 <50204829+seekr123@users.noreply.github.com>
2020-12-16 13:05:14 +0530

    Add files via upload

---
+++ class35HW/Virtual-Pet--1-master/js/Food.js
@@ -0,0 +1,44 @@
+class Food{
+    constructor(){
+        this.foodStock=0
+        this.lastFed;
+        this.milkImg=loadImage('images/Milk.png');
+    }
+
+   
+    
+    
+
+    getFoodStock(){
+        database.ref('Food').on('value',function(data){
+            this.foodStock=data.val();
+        });
+     }
+     updateFoodStock(count){
+        database.ref('/').update({
+            Food:count
+        })
+     }
+     
+
+     display(){
+         var x=80,y=100;
+         imageMode (CENTER);
+         image(this.milkImg,720,220,70,70);
+         console.log(this.foodStock);
+         
+         if(this.foodStock!=0){
+             for(var i=0;i<this.foodStock;i++){
+                     if(i%10==0){
+                     x=20;
+                     y=y+50
+                 }
+                 image(this.milkImg,x,y,50,50);
+                 x=x+20;
+             }
+         }
+
+     }
+
+   
+}
